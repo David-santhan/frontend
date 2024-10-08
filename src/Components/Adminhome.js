e62ec8efdd417da6769ad47b4eb1d5503048bc98
David-santhan <156925284+David-santhan@users.noreply.github.com>
2024-10-08 11:47:49 +0530

    Update Adminhome.js

--- src/Components/Adminhome.js
+++ src/Components/Adminhome.js
@@ -1,72 +1,32 @@
 import React, {  useState,useEffect, useRef } from 'react';
-// import AdminTopNav from './AdminTopNav';
+import AdminTopNav from './AdminTopNav';
 import Users from './Users';
-// import Button from 'react-bootstrap/Button';
-// import Card from 'react-bootstrap/Card';
-// import Image from 'react-bootstrap/Image';
-// import { useNavigate } from 'react-router-dom';
-// import Table from 'react-bootstrap/Table';
-// import { useSelector } from 'react-redux';
-// import UserTopNav from './UserTopNav';
+import UserTopNav from './UserTopNav';
+import CryptoJS from 'crypto-js';
+
 
 function Adminhome(){
-// useEffect(()=>{
-//   getUserList();
-// })
+  const JWT_SECRET = "ygsiahndCieqtkeresimsrcattoersmaigutiubliyellaueprtnernar";
 
+  const decryptData = (ciphertext, secret) => {
+    const bytes = CryptoJS.AES.decrypt(ciphertext, secret);
+    return bytes.toString(CryptoJS.enc.Utf8);
+  };
 
-// let storeObj = useSelector((store)=>{
-//   return store;
-// })
-// // console.log(storeObj);
-// // localStorage.setItem("Name1",storeObj.loginDetails.EmployeeName)
-// // localStorage.setItem("Employee Code1",storeObj.loginDetails.EmpCode)
-// // localStorage.setItem("Email1",storeObj.loginDetails.Email)
-// // localStorage.setItem("User Type1",storeObj.loginDetails.UserType)
-// // localStorage.setItem("ProfilePic1",storeObj.loginDetails.ProfilePic)
-// // localStorage.setItem("Status1",storeObj.loginDetails.Status)
+  const getDecryptedData = (key) => {
+    const encryptedValue = localStorage.getItem(key);
+    if (encryptedValue) {
+      return decryptData(encryptedValue, JWT_SECRET);
+    }
+    return null;
+  };
 
+  const localStoreStatusType = getDecryptedData("User Type");
 
-
-// let [search,setSearch]=useState('');
-//   let [userDetailsList,setUserDetailsList]=useState([]);
-//   let [selectedType,setSelectedType]=useState();
-
-//   let userTypeRef=useRef();
-//   let userStatusRef=useRef();
-
-//   let navigate = useNavigate();
-
-//   let getUserList=async()=>{
-//     let reqOption={
-//       method:"GET"
-//     }
-//     let JSONData= await fetch("http://localhost:7993/userDetailsHome",reqOption)
-//     let JSOData= await JSONData.json();
-//     setUserDetailsList(JSOData);   
-//   }
-//   let deleteUserDetails=async (id)=>{
-//     let reqOption={
-//         method:"DELETE",
-//     };   
-//  let JSONData=await fetch( `http://localhost:7993/deleteUser/${id}`,reqOption);
-//  let JSOData=await JSONData.json();
-//  if(JSOData.status==="success"){
-//     alert(JSOData.msg);
-//     window.location.reload();
-//     navigate("/Adminhome");
-//  }
-// }
-
-// let localStoreStatusType = localStorage.getItem("User Type");
-  return (
+return (
     <div className='bg'>
-    {/* {localStoreStatusType === "Admin" ? <AdminTopNav/> : <UserTopNav/>} */}
-    {/* <AdminTopNav/> */}
-    <Users/>
-      
-     
-      
+    {localStoreStatusType === "Admin" ? <AdminTopNav/> : <UserTopNav/>}
+   
     </div>
   )
 }
