140ef18beff64225222f4ac6590d3538c56cbdd4
David-santhan <156925284+David-santhan@users.noreply.github.com>
2024-10-24 11:00:33 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -67,26 +67,26 @@ function App() {
 
       <Route path='/forgotpassword' element={<Signup/>}></Route>
         <Route path='/home' element={<Home/>}></Route>
-        <Route path='/Adminhome' element={localStorageType === "Admin" ? <Adminhome/> : <Home/>}></Route>
-        <Route path='/newrequirements' element={<Newrequirements/>}></Route>
-        <Route path='/Adminaddnewrequirements' element={localStorageType === "Admin" ? <Adminaddnewrequirements/> : <Home/>}></Route>
-        <Route path='/AdminTopNav' element={<AdminTopNav/>}></Route>
-        <Route path='/AdminCreateUser' element={localStorageType === "Admin" ? <AdminCreateUser/> : <Home/>}></Route>
-        <Route path='/CreateClient' element={localStorageType === "Admin" ? <CreateClient/> : <Home/>}></Route>
+        <Route path='/Adminhome' element={localStorageType === "Admin" ? <Adminhome/> : localStorageType === "TeamLead" ? <TLHome/> :  <Home/>}></Route>
+        <Route path='/newrequirements' element={localStorageType === "Admin" ? <Adminaddnewrequirements/>: localStorageType === "TeamLead" ? <TLHome/> : <Home/>}></Route>
+        <Route path='/Adminaddnewrequirements' element={localStorageType === "Admin" ? <Adminaddnewrequirements/> : localStorageType === "TeamLead" ? <TLHome/> : <Home/>}></Route>
+        <Route path='/AdminTopNav' element={localStorageType === "Admin" ? <AdminTopNav/>: localStorageType === "TeamLead" ? <TeamLeadTopNav/> : <UserTopNav/>}></Route>
+        <Route path='/AdminCreateUser' element={localStorageType === "Admin" ? <AdminCreateUser/>: localStorageType === "TeamLead" ? <TLHome/> : <Home/>}></Route>
+        <Route path='/CreateClient' element={localStorageType === "Admin" ? <CreateClient/> : localStorageType === "TeamLead" ? <TLHome/> : <Home/>}></Route>
         <Route path='/ResetPassword/:id/:token' element={<ResetPassword/>}></Route>
         <Route path='/Profile'  element={<Profile/>}></Route>
-        <Route path='/Clients' element={<Clients/>}></Route>
-        <Route path='/Users' element={<Users/>}></Route>
+        <Route path='/Clients' element={localStorageType === "Admin" ? <Clients/>: localStorageType === "TeamLead" ? <TLHome/> : <Home/>}></Route>
+        <Route path='/Users' element={localStorageType === "Admin" ? <Users/>: localStorageType === "TeamLead" ? <TLHome/> : <Home/>}></Route>
         <Route path='/ClientProfile/:id' element={<ClientProfile/>}></Route>
         <Route path='/UserTopNav' element={<UserTopNav/>}></Route>
-        <Route path='/UptadeClient/:id' element={localStorageType === "Admin" ? <AdminUpdateClient/> : <Home/>}></Route>
-        <Route path='/UpdateUser/:id' element={localStorageType === "Admin" ? <UpdateUser/> : <Home/>}></Route>
-        <Route path='/Requirments' element={localStorageType === "Admin" ? <Requirments/> : <Home/>}></Route>
+        <Route path='/UptadeClient/:id' element={localStorageType === "Admin" ? <AdminUpdateClient/> : localStorageType === "TeamLead" ? <TLHome/> : <Home/>}></Route>
+        <Route path='/UpdateUser/:id' element={localStorageType === "Admin" ? <UpdateUser/> : localStorageType === "TeamLead" ? <TLHome/> : <Home/>}></Route>
+        <Route path='/Requirments' element={localStorageType === "Admin" ? <Requirments/> : localStorageType === "TeamLead" ? <TLHome/>  : <Home/>}></Route>
         <Route path='/UserAction/:id/:user' element={<UserActions/>}></Route>
-        <Route path='/TeamLeadTopNav' element={<TeamLeadTopNav/>}></Route>
-        <Route path='/TLHome' element={<TLHome/>}></Route>
+        <Route path='/TeamLeadTopNav' element={localStorageType === "Admin" ? <AdminTopNav/>: localStorageType === "TeamLead" ? <TeamLeadTopNav/> : <UserTopNav/>}></Route>
+        <Route path='/TLHome' element={localStorageType === "TeamLead" ? <TLHome/> : <Home/>}></Route>
         <Route path='/TeamClient' element={<TeamClients/>}></Route>
-        <Route path='/TLNewReq' element={<TLNewRequirement/>}></Route>
+        <Route path='/TLNewReq' element={ localStorageType === "TeamLead" ? <TLNewRequirement/> : <Home/>}></Route>
         <Route path='*' element={<div>Page Not Found</div>}></Route>
       </Routes>
       
